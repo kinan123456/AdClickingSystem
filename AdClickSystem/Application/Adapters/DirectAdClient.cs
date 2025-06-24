0e1a6a16ae571fc60fdd10e4cfcd7e88dce00915
Kinan <kinansobih29@gmail.com>
2025-06-24 15:10:55 +0300

    Let DirectAdClient call the real methods of the AdService

--- AdClickSystem/Application/Adapters/DirectAdClient.cs
+++ AdClickSystem/Application/Adapters/DirectAdClient.cs
@@ -1,22 +1,45 @@
-﻿using Domain.Entities.AdClickSystem.Domain.Entities;
+﻿using Application.Services;
+using Domain.Entities.AdClickSystem.Domain.Entities;
+using Serilog;
 
 namespace Application.Adapters
 {
+    /// <summary>
+    /// Direct implementation of an ad client adapter.
+    /// This class calls the IAdService methods directly.
+    /// If you later switch to an HTTP or gRPC adapter, simply implement IAdClient accordingly.
+    /// </summary>
     public class DirectAdClient : IAdClient
     {
-        public Task ClickAdAsync(Guid adId)
+        private readonly IAdService _adService;
+        private readonly ILogger _logger;
+
+        public DirectAdClient(IAdService adService, ILogger logger)
         {
-            return Task.CompletedTask;
+            _adService = adService;
+            _logger = logger;
         }
 
-        public Task<Ad> GetAdAsync(string language, string country, string adSize)
+        public async Task ClickAdAsync(Guid adId)
         {
-            return Task.FromResult(new Ad { });
+            _adService.ClickAd(adId);
+            _logger.Information("DirectAdClient: ClickAdAsync recorded click for ad {AdId}", adId);
+            await Task.CompletedTask;
         }
 
-        public Task RegisterAdAsync(Guid adId)
+        public async Task<Ad> GetAdAsync(string language, string country, string adSize)
         {
-            return Task.CompletedTask;
+            var ad = _adService.GetAd(language, country, adSize);
+            _logger.Information("DirectAdClient: GetAdAsync returned ad {AdId} for {Language}-{Country}-{AdSize}",
+                ad.AdId, language, country, adSize);
+            return await Task.FromResult(ad);
+        }
+
+        public async Task RegisterAdAsync(Guid adId)
+        {
+            _adService.RegisterAd(adId);
+            _logger.Information("DirectAdClient: RegisterAdAsync recorded registration for ad {AdId}", adId);
+            await Task.CompletedTask;
         }
     }
 }
